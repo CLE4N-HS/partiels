84593a4c11df1af42851b0409db16c7bc0e8ddcc
CLE4N-HS <cleen.noe@gmail.com>
2024-04-02 16:37:21 +0200

    teddy malou

--- CSFML_BaseProjectFU/CSFML_BaseProjectFU/finish.c
+++ CSFML_BaseProjectFU/CSFML_BaseProjectFU/finish.c
@@ -2,6 +2,7 @@
 #include "viewManager.h"
 #include "CustomMath.h"
 #include "map.h"
+#include "textureManager.h"
 
 typedef struct {
 	sfView* view;
@@ -16,9 +17,43 @@ sfVector2f astronautFinishScale;
 
 int nbDisplay;
 
+sfSprite* starSprite;
+sfTexture* starTexture;
+
+sfShader* starShader;
+sfRenderStates starRenderState;
+
+sfBool isShaderOk;
+
+sfGlslVec2 res = { 1920, 1080 };
+
+float saveRotatePos;
 
 void initFinsih()
 {
+	starSprite = sfSprite_create();
+	starTexture = GetTexture("star");
+	sfSprite_setTexture(starSprite, starTexture, sfTrue);
+	sfSprite_setOrigin(starSprite, vector2f(200.f, 200.f));
+
+	if (sfShader_isAvailable())
+	{
+		isShaderOk = sfTrue;
+
+		starShader = sfShader_createFromFile(NULL, NULL, "../Ressources/Shaders/star.frag");
+
+		starRenderState.shader = starShader;
+		starRenderState.blendMode = sfBlendAlpha;
+		starRenderState.texture = starTexture;
+		starRenderState.transform = sfTransform_Identity;
+ 
+	}
+	else
+	{
+		isShaderOk = sfFalse;
+	}
+
+
 	for (int i = 0; i < 2; i++)
 	{
 		v[i].view = sfView_create();
@@ -32,6 +67,8 @@ void initFinsih()
 	finishTimer = 0.f;
 	frogFinishScale = vector2f(PLAYER_SCALE, PLAYER_SCALE);
 	astronautFinishScale = vector2f(PLAYER_SCALE, PLAYER_SCALE);
+
+	saveRotatePos = 0.f;
 
 	nbDisplay = 0;
 }
@@ -61,6 +98,27 @@ void displayFinish(Window* _window)
 {
 	if (nbDisplay < 0 || nbDisplay > 1)
 		return;
+
+	if (isShaderOk) {
+		float fTimer = finishTimer / FINISH_TIMER_DURATION;
+		for (int i = 0; i < 2; i++)
+		{
+			if (fTimer < 0.5f) {
+				sfSprite_setScale(starSprite, vector2f(Lerp(0.f, 1.f, fTimer * 2.f), Lerp(0.f, 1.f, fTimer * 2.f)));
+				sfSprite_setRotation(starSprite, finishTimer * 90.f);
+				sfSprite_setRotation(starSprite, 0.f + finishTimer * finishTimer * finishTimer * 10.f);
+				saveRotatePos = finishTimer * 90.f;
+			}
+			else {
+				sfSprite_setScale(starSprite, vector2f(Lerp(1.f, 0.f, (fTimer - 0.5f) * 2), Lerp(1.f, 0.f, (fTimer - 0.5f) * 2.f)));
+				sfSprite_setRotation(starSprite, 0.f + finishTimer * finishTimer  * finishTimer * 10.f);
+			}
+			sfSprite_setPosition(starSprite, getFinishPlayerPos(i));
+			sfShader_setTextureUniform(starShader, "u_texture", starTexture);
+			sfShader_setFloatUniform(starShader, "u_timer", fTimer);
+			sfRenderTexture_drawSprite(_window->renderTexture, starSprite, &starRenderState);
+		}
+	}
 
 	sfView_setCenter(v[nbDisplay].view, v[nbDisplay].pos);
 	sfView_setSize(v[nbDisplay].view, v[nbDisplay].size);
